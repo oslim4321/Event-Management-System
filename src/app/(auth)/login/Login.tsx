0ecba7f4b3f899e7bee596a14949dc041f655d4f
sooto selim <adewaleselim6@gmail.com>
2023-08-05 09:44:29 +0100

    login and signup component

---
+++ src/app/(auth)/login/Login.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      {/* <!-- component --> */}
+
+      {/* <!-- login content --> */}
+            <div className="space-y-4">
+                <header className="mb-3 text-2xl font-bold">Log in</header>
+                <div className="w-full rounded-2xl bg-gray-50 px-4 ring-2 ring-gray-200 focus-within:ring-blue-400">
+                    <input type="text" placeholder="Email or username" className="my-3 w-full border-none bg-transparent outline-none focus:outline-none" />
+                </div>
+                <div className="flex w-full items-center space-x-2 rounded-2xl bg-gray-50 px-4 ring-2 ring-gray-200 focus-within:ring-blue-400">
+                    <input type="password" placeholder="Password" className="my-3 w-full border-none bg-transparent outline-none" />
+                    <a href="#" className="font-medium text-gray-400 hover:text-gray-500">FORGOT?</a>
+                </div>
+                <button className="w-full rounded-2xl border-b-4 border-b-blue-600 bg-blue-500 py-3 font-bold text-white hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400">LOG IN</button>
+            </div>
+</div>
+  )
+}
+
+export default Login
