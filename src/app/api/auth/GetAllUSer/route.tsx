0b35a8d51a913dc2f4b86e3ce00cf5918d685f16
sooto selim <adewaleselim6@gmail.com>
2023-08-08 22:16:17 +0100

    remove the get al user route

--- src/app/api/auth/GetAllUSer/route.tsx
+++
@@ -1,8 +0,0 @@
-import User from "@/model/User";
-import { NextResponse } from "next/server";
-
-
-export const GET = async (request: Request) => {
-    const user = await User.find()
-    return NextResponse.json({ user })
-};
