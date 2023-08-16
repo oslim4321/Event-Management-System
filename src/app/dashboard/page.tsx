5046af5fa0c638d80b8e843bce51ddb47e6f57e3
sooto selim <adewaleselim6@gmail.com>
2023-08-16 16:39:43 +0100

    fix the issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,85 +1,96 @@
+// import React from 'react'
+// import axios from 'axios'
+// import { getServerSession } from "next-auth"
+// import { handler } from '../api/auth/[...nextauth]/route'
+// import LogoutButton from '@/components/LogoutButton'
+// // import ListMyEvent from './ListMyEvent'
+
+// const getData = async () => {
+//     try {
+//         const session: any = await getServerSession(handler)
+//         if (!session) {
+//             throw Error('No session please login')
+
+//         }
+//         const email: string = session?.user?.email
+//         const data: any = await axios.post(process.env.BASE_URL + '/api/auth/getUser', { data: { email } })
+//         // console.log(data.message, 'mee')
+//         return data?.data?.message
+//     } catch (error) {
+//         throw Error('failed fetch data on')
+//     }
+// }
+// export const metadata = {
+//     title: "Dashboard page",
+//     description: "this is oslim Event manager Dashboard page",
+// };
+
+// const page = async () => {
+//     const data = await getData()
+//     return (
+//         <div>
+
+//             {data._id && <div className='max-w-[80%] mx-auto'>
+//                 <div className="md:grid grid-cols-4 grid-rows-2  bg-white gap-2 p-4 rounded-xl">
+//                     <div className="md:col-span-1 h-48 shadow-xl ">
+//                         <div className="flex w-full h-full relative">
+//                             <img src="https://res.cloudinary.com/dboafhu31/image/upload/v1625318266/imagen_2021-07-03_091743_vtbkf8.png" loading='lazy' className="w-44 h-44 m-auto" alt="" />
+
+//                         </div>
+//                     </div>
+//                     <div className="relative md:col-span-3 h-48 shadow-xl p-4 space-y-2 p-3">
+//                         <div className='absolute right-10'>
+
+//                             <LogoutButton />
+//                         </div>
+//                         <div className="flex ">
+//                             <span
+//                                 className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Name:</span>
+//                             <input
+//                                 className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+//                                 type="text" value={data?.firstName + ' ' + data?.lastName} readOnly />
+//                         </div>
+//                         <div className="flex ">
+//                             <span
+//                                 className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Email:</span>
+//                             <input
+//                                 className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+//                                 type="text" value={data?.email} readOnly />
+//                         </div>
+//                         {/* <div className="flex ">
+//                             <span
+//                                 className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Role:</span>
+//                             <input
+//                                 className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+//                                 type="text" value="Admin" readOnly />
+//                         </div> */}
+//                     </div>
+//                     <div className="md:col-span-3 h-48 shadow-xl p-4 space-y-2 hidden md:block">
+//                         <h3 className="font-bold uppercase"> Profile Description</h3>
+//                         <p className="">
+//                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget laoreet diam, id luctus lectus. Ut consectetur nisl ipsum, et faucibus sem finibus vitae. Maecenas aliquam dolor at dignissim commodo. Etiam a aliquam tellus, et suscipit dolor. Proin auctor nisi velit, quis aliquet sapien viverra a.
+//                         </p>
+//                     </div>
+
+//                 </div>
+//             </div>}
+
+//             {/* <ListMyEvent /> */}
+
+
+
+//         </div>
+//     )
+// }
+
+// export default page
+
+
 import React from 'react'
-import axios from 'axios'
-import { getServerSession } from "next-auth"
-import { handler } from '../api/auth/[...nextauth]/route'
-import LogoutButton from '@/components/LogoutButton'
-// import ListMyEvent from './ListMyEvent'
 
-const getData = async () => {
-    try {
-        const session: any = await getServerSession(handler)
-        if (!session) {
-            throw Error('No session please login')
-
-        }
-        const email: string = session?.user?.email
-        const data: any = await axios.post(process.env.BASE_URL + '/api/auth/getUser', { data: { email } })
-        // console.log(data.message, 'mee')
-        return data?.data?.message
-    } catch (error) {
-        throw Error('failed fetch data on')
-    }
-}
-export const metadata = {
-    title: "Dashboard page",
-    description: "this is oslim Event manager Dashboard page",
-};
-
-const page = async () => {
-    const data = await getData()
+const page = () => {
     return (
-        <div>
-
-            {data._id && <div className='max-w-[80%] mx-auto'>
-                <div className="md:grid grid-cols-4 grid-rows-2  bg-white gap-2 p-4 rounded-xl">
-                    <div className="md:col-span-1 h-48 shadow-xl ">
-                        <div className="flex w-full h-full relative">
-                            <img src="https://res.cloudinary.com/dboafhu31/image/upload/v1625318266/imagen_2021-07-03_091743_vtbkf8.png" loading='lazy' className="w-44 h-44 m-auto" alt="" />
-
-                        </div>
-                    </div>
-                    <div className="relative md:col-span-3 h-48 shadow-xl p-4 space-y-2 p-3">
-                        <div className='absolute right-10'>
-
-                            <LogoutButton />
-                        </div>
-                        <div className="flex ">
-                            <span
-                                className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Name:</span>
-                            <input
-                                className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
-                                type="text" value={data?.firstName + ' ' + data?.lastName} readOnly />
-                        </div>
-                        <div className="flex ">
-                            <span
-                                className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Email:</span>
-                            <input
-                                className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
-                                type="text" value={data?.email} readOnly />
-                        </div>
-                        {/* <div className="flex ">
-                            <span
-                                className="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Role:</span>
-                            <input
-                                className="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
-                                type="text" value="Admin" readOnly />
-                        </div> */}
-                    </div>
-                    <div className="md:col-span-3 h-48 shadow-xl p-4 space-y-2 hidden md:block">
-                        <h3 className="font-bold uppercase"> Profile Description</h3>
-                        <p className="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget laoreet diam, id luctus lectus. Ut consectetur nisl ipsum, et faucibus sem finibus vitae. Maecenas aliquam dolor at dignissim commodo. Etiam a aliquam tellus, et suscipit dolor. Proin auctor nisi velit, quis aliquet sapien viverra a.
-                        </p>
-                    </div>
-
-                </div>
-            </div>}
-
-            {/* <ListMyEvent /> */}
-
-
-
-        </div>
+        <div>page</div>
     )
 }
 
