97f1064f7893651d478bd0cd78237f1231fc5397
selim Adewale <adewaleselim6@gmail.com>
2023-10-03 22:11:14 +0100

    fix the annoying overflow

--- src/components/AboutEv.tsx
+++ src/components/AboutEv.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 import React from "react";
 import { ImgComp } from "./ImageComp";
-import styles from "./Styles.module.css"; // Import your CSS file
 import { useInView } from "react-intersection-observer";
+import styles from "./Styles.module.css"; // Import your CSS file
 
 const AboutEv = () => {
   const [ref, inView] = useInView({
@@ -11,22 +11,26 @@ const AboutEv = () => {
 
   return (
     <div
-      className={`w-full md:h-[70vh] h-[100vh] bg-light flex justify-center items-center`}
+      className={`w-full md:h-[70vh] h-[100vh] bg-light flex justify-center items-center overflow-hidden`}
       ref={ref}
     >
       <div className="container flex flex-col md:flex-row">
         <div
-          className={`w-full md:w-1/2 inline-flex  justify-center items-center md:p-0 p-5 ${inView ? styles["image-animation"] : ""}`}
+          className={`w-full md:w-1/2 inline-flex  justify-center items-center md:p-0 p-5 ${
+            inView ? styles["image-animation"] : ""
+          }`}
         >
           <ImgComp src="/image53.jpg" alt="event" />
         </div>
         <div
-          className={`w-full md:w-1/2 flex flex-col justify-center items-center p-5 space-y-8 ${inView ? styles["text-animation"] : ""}`}
+          className={`w-full md:w-1/2 flex flex-col justify-center items-center p-5 space-y-8 ${
+            inView ? styles["text-animation"] : ""
+          }`}
         >
           <h1 className="text-left text-3xl font-canv">ABOUT EVENTHORIZON</h1>
           <p className="text-left text-lg font-canv">
-            At EventHorizon, we believe that every event has the potential to
-            be extraordinary. Founded with a passion for creativity and an
+            At EventHorizon, we believe that every event has the potential to be
+            extraordinary. Founded with a passion for creativity and an
             unwavering commitment to excellence, we have dedicated ourselves to
             the art of event management.
           </p>
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,36 +1,58 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import { ImgComp } from "./ImageComp";
 import styles from "./Styles.module.css";
 import { useInView } from "react-intersection-observer";
 const Story = () => {
-    const [ref, inView] = useInView({
-        threshold: 0.5, // Adjust this threshold as needed
-    });
+  const [ref, inView] = useInView({
+    threshold: 0.5, // Adjust this threshold as needed
+  });
 
   return (
-  
-    
-    <div className="w-full md:h-[70vh] h-[150vh] bg-blue-800 flex justify-center items-center" ref={ref} >
+    <div
+      className="w-full md:h-[70vh] h-[150vh] bg-blue-800 flex justify-center items-center overflow-hidden"
+      ref={ref}
+    >
       <div className="container flex flex-col md:flex-row flex-wrap">
         <div className="w-full md:w-1/2 p-4 flex justify-center items-center">
-          <div className={` bg-white w-full h-[100%] md:max-w-md p-8 rounded-lg shadow-lg space-y-5 ${inView ? styles["image-animation"] : ""}`}>
-            <ImgComp src="/story.jpg" alt="event" width="100px" height='100px' className='mx-auto rounded'></ImgComp>
+          <div
+            className={` bg-white w-full h-[100%] md:max-w-md p-8 rounded-lg shadow-lg space-y-5 ${
+              inView ? styles["image-animation"] : ""
+            }`}
+          >
+            <ImgComp
+              src="/story.jpg"
+              alt="event"
+              width="100px"
+              height="100px"
+              className="mx-auto rounded"
+            ></ImgComp>
             <h2 className="text-3xl font-semibold mb-4 font-canv text-center">
               OUR STORY
             </h2>
             <p className="font-canv">
               Our journey began with a simple yet profound idea: to transform
               ordinary gatherings into unforgettable moments. With years of
-              experience in the event industry, we've honed our skills, perfected
-              our craft, and built a team of talented professionals who share our
-              vision. This is the content of Card 1. You can customize it as needed.
+              experience in the event industry, we've honed our skills,
+              perfected our craft, and built a team of talented professionals
+              who share our vision. This is the content of Card 1. You can
+              customize it as needed.
             </p>
           </div>
         </div>
         <div className="w-full md:w-1/2 p-4 flex justify-center items-center">
-          <div  className={` bg-white w-full h-[100%] md:max-w-md p-8 rounded-lg shadow-lg space-y-5 ${inView ? styles["text-animation"] : ""}`}>
-            <ImgComp src="/mission.jpg" alt="event"  width="100px" height='100px' className='mx-auto rounded'></ImgComp>
+          <div
+            className={` bg-white w-full h-[100%] md:max-w-md p-8 rounded-lg shadow-lg space-y-5 ${
+              inView ? styles["text-animation"] : ""
+            }`}
+          >
+            <ImgComp
+              src="/mission.jpg"
+              alt="event"
+              width="100px"
+              height="100px"
+              className="mx-auto rounded"
+            ></ImgComp>
             <h2 className="text-3xl font-semibold mb-4 font-canv text-center">
               OUR MISSION
             </h2>
