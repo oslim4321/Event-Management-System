7ff4c2794a4920aad986e13764c6073579992e17
Harmedino <adamsahmed191@gmail.com>
2023-10-05 12:51:55 +0100

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import TestReduc from "./TestReduc";
 import Herosection from "@/components/Herosection";
 import AboutEv from "@/components/AboutEv";
 import Story from "@/components/Story";
+import Choose from "@/components/Choose";
 
 export default function Home() {
   return (
     <main className="">
       <Herosection />
       <AboutEv />
-      <Story/>     {/* <TestReduc/> */}
+      <Story />
+      <Choose/>
+      {/* <TestReduc/> */}
     </main>
   );
 }
