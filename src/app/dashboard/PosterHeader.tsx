ee6187aacc242206839ff26da16a64d49402af93
selim Adewale <adewaleselim6@gmail.com>
2023-10-05 19:03:35 +0100

    working on the dahboard

---
+++ src/app/dashboard/PosterHeader.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const PosterHeader = () => {
+  return (
+    <header className="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
+      <div className="flex items-center">
+        <button className="text-gray-500 focus:outline-none lg:hidden">
+          <svg
+            className="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 6H20M4 12H20M4 18H11"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </button>
+
+        <div className="relative mx-4 lg:mx-0">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              className="w-5 h-5 text-gray-500"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </span>
+
+          <input
+            className="w-32 pl-10 pr-4 rounded-md form-input sm:w-64 focus:border-indigo-600"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PosterHeader;
--- src/components/SignUpWithGoggleCompo.tsx
+++ src/components/SignUpWithGoggleCompo.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { signIn } from 'next-auth/react'
-import React from 'react'
-import { useSession } from 'next-auth/react'
+"use client";
+import { signIn } from "next-auth/react";
+import React from "react";
+import { useSession } from "next-auth/react";
 
 const SignUpWithGoggleCompo = () => {
   const session = useSession();
@@ -13,17 +13,25 @@ const SignUpWithGoggleCompo = () => {
         <hr className="w-full border border-gray-300" />
       </div>
       <footer>
-        <div className="grid grid-cols-2 gap-4">
+        {/* <div className="grid grid-cols-2 gap-4">
           <a href="#" className="rounded-2xl border-b-2 border-b-gray-300 bg-white px-4 py-2.5 font-bold text-blue-700 ring-2 ring-gray-300 hover:bg-gray-200 active:translate-y-[0.125rem] active:border-b-gray-200">FACEBOOK</a>
           <div onClick={() => signIn("google")} className="rounded-2xl border-b-2 border-b-gray-300 bg-white px-4 py-2.5 font-bold text-blue-500 ring-2 ring-gray-300 hover:bg-gray-200 active:translate-y-[0.125rem] active:border-b-gray-200">GOOGLE</div>
-        </div>
+        </div> */}
 
         <div className="mt-8 text-sm text-gray-400">
           By signing in to ********, you agree to our
-          <a href="#" className="font-medium text-gray-500">Terms</a> and <a href="#" className="font-medium text-gray-500">Privacy Policy</a>.
+          <a href="#" className="font-medium text-gray-500">
+            Terms
+          </a>{" "}
+          and{" "}
+          <a href="#" className="font-medium text-gray-500">
+            Privacy Policy
+          </a>
+          .
         </div>
-      </footer></div>
-  )
-}
+      </footer>
+    </div>
+  );
+};
 
-export default SignUpWithGoggleCompo
+export default SignUpWithGoggleCompo;
