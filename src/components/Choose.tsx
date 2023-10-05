f7012f2ab503fa811f846b5181bb219982be4718
Harmedino <adamsahmed191@gmail.com>
2023-10-05 12:52:23 +0100

    trying

---
+++ src/components/Choose.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Choose = () => {
+  return (
+      <div className='w-full md:h-[90vh] h-[100vh] bg-light '>
+          
+          <h1 className='text-center font-canv text-3xl'>Why Choose EventHorizon</h1>
+
+          <div className="flex flex-col md:flex-row container w-full border justify-around mx-auto items-center space-y-4 md:space-y-0 md:space-x-4">
+  <div className="w-full md:w-80 p-4">
+    <div className="bg-white rounded-lg shadow-lg">
+      <img
+        src="/story.jpg"
+        alt="Expertise 1"
+        className="w-full h-40 object-cover rounded-t-lg"
+      />
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-center">Expertise 1</h2>
+        <p className="text-gray-700 mt-2 text-center">
+          Our team brings a wealth of experience and expertise to the table. We've successfully orchestrated a wide range of events, from intimate gatherings to large-scale extravaganzas.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div className="w-full md:w-80 p-4">
+    <div className="bg-white rounded-lg shadow-lg">
+      <img
+        src="/story.jpg"
+        alt="Expertise 2"
+        className="w-full h-40 object-cover rounded-t-lg"
+      />
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-center">Expertise 2</h2>
+        <p className="text-gray-700 mt-2 text-center">
+          Our team brings a wealth of experience and expertise to the table. We've successfully orchestrated a wide range of events, from intimate gatherings to large-scale extravaganzas.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Choose
